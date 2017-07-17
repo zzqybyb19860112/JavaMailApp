11a185b0882d9493ee256d81f6268de07d7d9940
zzqybyb1986 <417986033@qq.com>
2017-07-17 13:30:55 +0800

    提交试用JavaMail框架编写的demo代码

---
+++ app/src/main/java/com/ybyb/zzq/javamailapp/AppConstants.java
@@ -0,0 +1,12 @@
+package com.ybyb.zzq.javamailapp;
+
+/**
+ * 作者：周正权 on 2017/7/14
+ * 邮箱：zhouzhengquan@shixinyun.com
+ */
+
+public class AppConstants {
+    public static final int TYPE_IMAP=1;
+    public static final int TYPE_POP=2;
+    public static final int TYPE_STMP=3;
+}
